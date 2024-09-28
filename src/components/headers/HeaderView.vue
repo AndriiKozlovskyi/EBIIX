e84d24c098f8698d28d49eaf693c5b7150145c83
AndriiKozlovskyi <andrii@software.com>
2024-09-29 00:22:08 +0200

    name changed

--- src/components/headers/HeaderView.vue
+++ src/components/headers/HeaderView.vue
@@ -2,7 +2,7 @@
     <nav class="z-1000 flex flex-col md:flex-row md:justify-between items-center bg-[#ed9121]/80 w-full px-4 py-1 text-white shadow-md space-y-4 md:space-y-0">
         <!-- Logo Section (Left) -->
         <div class="flex flex-col items-start w-full md:w-1/3 md:justify-start order-1 md:order-1">
-            <p class="text-lg font-semibold">EBIIX Service Rowerowy</p>
+            <p class="text-lg font-semibold">EBIIX Serwis Rowerowy</p>
             <p class="text-sm text-slate-200">E-Bike Fix</p>
         </div>
 
